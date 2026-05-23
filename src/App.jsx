f8dd703d01dca29143a73d372461afd7ea770bf3
RbsDeveloper <developer.rbs@gmail.com>
2026-05-24 01:03:19 +0300

    style(preview): add temporary base styles for data rendering verification

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import NavBar from './components/nav'
 import CvEditor from './components/CvEditor'
 import CvPreview from './components/CvPreview'
 import { useState } from 'react'
-//import './App.css'
+import './App.css'
 
 const sampleCv = {
     personalInfo: {
-      profileImgUrl: "https://pixabay.com/photos/canoe-lake-adventure-kayak-2179196/",
+      profileImgUrl: "https://images.pexels.com/photos/15395870/pexels-photo-15395870.jpeg?_gl=1*114wdao*_ga*MTY3ODM2MTkxMy4xNzY5ODkyNjA4*_ga_8JE65Q40S6*czE3Nzk1Njk3NTgkbzIkZzEkdDE3Nzk1Njk4MzQkajU5JGwwJGgw",
       fullName: "Alex Morgan",
       email: "alex.morgan@email.com",
       phone: "+1 (555) 019-2834",
--- src/components/CvPreview.jsx
+++ src/components/CvPreview.jsx
@@ -1,116 +1,126 @@
+import { Mail, Phone, Globe, MapPin, Link  } from "lucide-react";
+
+
 function CvPreview({cvInfo}) {
     return (
         <div className="preview-wrapper">
             <article className="a4-page">
                 <header>
-                    <h1>{cvInfo.personalInfo.fullName}</h1>
                     {cvInfo.personalInfo.profileImgUrl && <div className="profileWrapper"><img src={cvInfo.personalInfo.profileImgUrl} alt="" /></div>}
                     <address>
-                        <ul>
-                            <li><a href="mailto:...">{cvInfo.personalInfo.email}</a></li>
-                            <li><a href="tel:...">{cvInfo.personalInfo.phone}</a></li>
-                            <li>{cvInfo.personalInfo.location}</li>
-                            <li><a href={cvInfo.personalInfo.linkedInUrl}>{cvInfo.personalInfo.linkedInUrl}</a></li>
-                            <li><a href={cvInfo.personalInfo.websiteUrl}>{cvInfo.personalInfo.websiteUrl}</a></li>
+                        <h1>{cvInfo.personalInfo.fullName}</h1>
+                        <ul className="contactList">
+                            <li><Mail className="contactIcon"></Mail><a href="mailto:...">{cvInfo.personalInfo.email}</a></li>
+                            <li><Phone className="contactIcon"></Phone><a href="tel:...">{cvInfo.personalInfo.phone}</a></li>
+                            <li><MapPin className="contactIcon"></MapPin>{cvInfo.personalInfo.location}</li>
+                            <li><Link className="contactIcon"></Link><a href={cvInfo.personalInfo.linkedInUrl}>{cvInfo.personalInfo.linkedInUrl}</a></li>
+                            <li><Globe className="contactIcon"></Globe><a href={cvInfo.personalInfo.websiteUrl}>{cvInfo.personalInfo.websiteUrl}</a></li>
                         </ul>
                     </address>
                 </header>
-                <section id="summary">
-                    <p>{cvInfo.personalInfo.summary}</p>
-                </section>
-                <section id="experience">
-                    <h2>Work experience</h2>
-                    <ul>
-                        {cvInfo.jobs.map(job=>{
-                            return (
-                                <li key={job.id}>
-                                    <div className="jobHeader">
-                                        <div>
-                                            <h3>{job.position}</h3>
-                                            <strong>{job.company}</strong>
-                                        </div>
-                                        <div>
-                                            <time>{job.startDate}</time> - <time>{job.workHere ? 'Present' : job.endDate}</time>
-                                            <span>{job.location}</span>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <p>{job.description}</p>
-                                    </div>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </section>
-                <section id="education">
-                    <h2>Education</h2>
-                    <ul>
-                        {
-                            cvInfo.education.map(institute=>{
+                <div className="cvBody">
+                    <section id="summary">
+                        <h2>Professional Summary</h2>
+                        <p>{cvInfo.personalInfo.summary}</p>
+                    </section>
+                    <section id="experience">
+                        <h2>Work experience</h2>
+                        <ul className="jobList">
+                            {cvInfo.jobs.map(job=>{
                                 return (
-                                    <li key={institute.id}>
-                                        <div className="studyHeader">
+                                    <li key={job.id}>
+                                        <div className="jobHeader">
                                             <div>
-                                                <h3>{institute.degree}</h3>
-                                                <strong>{institute.institution}</strong>
+                                                <h3>{job.position}</h3>
+                                                <strong>{job.company}</strong>
                                             </div>
-                                            <div>
-                                                <time>{institute.startDate}</time> - <time>{institute.endDate ? institute.endDate : "Present"}</time>
-                                                <span>{institute.location}</span>
+                                            <div className="timeline">
+                                                <div className="period">
+                                                    <time>{job.startDate}</time> - <time>{job.workHere ? 'Present' : job.endDate}</time>
+                                                </div>
+                                                <span className="location">{job.location}</span>
                                             </div>
-                                            <div>
-                                                <p>{institute.description}</p>
-                                            </div>
+                                        </div>
+                                        <div>
+                                            <p>{job.description}</p>
                                         </div>
                                     </li>
                                 )
-                            })
-                        }
-                    </ul>
-                </section>
-                <section id="skills">
-                    <h2>Skills</h2>
-                    <ul>
-                        {
-                            cvInfo.skills.map(skill=>{
-                                return (
-                                    <li key={skill.id}>
-                                       <span className="skillCategory">{skill.category}</span> 
-                                       {skill.skillsList} 
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </section>
-                <section id="projects">
-                        <h2>Projects</h2>
-                        <ul>
+                            })}
+                        </ul>
+                    </section>
+                    <section id="education">
+                        <h2>Education</h2>
+                        <ul className="educationList">
                             {
-                                cvInfo.projects.map(project=>{
+                                cvInfo.education.map(institute=>{
                                     return (
-                                        <li key={project.id}>
-                                            <h3>{project.projectName}</h3>
-                                            <p className="projectDescription">{project.description}</p>
-                                            <p><span>Technologies: </span>{project.tech.join(", ")}</p>
+                                        <li key={institute.id}>
+                                            <div className="studyHeader">
+                                                <div>
+                                                    <h3>{institute.degree}</h3>
+                                                    <strong>{institute.institution}</strong>
+                                                </div>
+                                                <div className="timeline">
+                                                    <div className="period">
+                                                        <time>{institute.startDate}</time> - <time>{institute.endDate ? institute.endDate : "Present"}</time>
+                                                    </div>
+                                                    <span className="location">{institute.location}</span>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                    <p>{institute.description}</p>
+                                                </div>
                                         </li>
                                     )
                                 })
                             }
                         </ul>
-                </section>
-                <section>
-                    <h2>Languages</h2>
-                    <ul>
-                        {cvInfo.languages.map(language=> {
-                            return (
-                                <li key={language.id}>
-                                    <p><span>{language.name}</span>-{language.proficiency}</p>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </section>
+                    </section>
+                    <section id="skills">
+                        <h2>Skills</h2>
+                        <ul className="skillsList">
+                            {
+                                cvInfo.skills.map(skill=>{
+                                    return (
+                                        <li key={skill.id}>
+                                        <span className="skillCategory">{skill.category}: </span> 
+                                        {skill.skillsList} 
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </section>
+                    <section id="projects">
+                            <h2>Projects</h2>
+                            <ul className="projectsList">
+                                {
+                                    cvInfo.projects.map(project=>{
+                                        return (
+                                            <li key={project.id}>
+                                                <h3>{project.projectName}</h3>
+                                                <p className="projectDescription">{project.description}</p>
+                                                <p><span>Technologies: </span>{project.tech.join(", ")}</p>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                    </section>
+                    <section>
+                        <h2>Languages</h2>
+                        <ul className="languagesList">
+                            {cvInfo.languages.map(language=> {
+                                return (
+                                    <li key={language.id}>
+                                        <p><span className="languageName">{language.name}</span> - {language.proficiency}</p>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </section>
+                </div>
             </article>
         </div>
     )
